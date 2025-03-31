89fc0823c54b3f508792adb135434ced93639bed
Yuseeeee <49379138@est.ort.edu.ar>
2025-03-31 10:48:38 -0300

    a

---
+++ Assets/Scripts/Num1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Num1 : MonoBehaviour
+{
+    int num1 = 0;
+    int resultado;
+    int multiplicacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = num1 + 2;
+        Debug.Log("La suma de " + num1 + " y " + "2" + " es: " + resultado);
+        multiplicacion = resultado * resultado;
+        Debug.Log("La multiplicacion de " + resultado + " y " + resultado + " es: " + multiplicacion);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
