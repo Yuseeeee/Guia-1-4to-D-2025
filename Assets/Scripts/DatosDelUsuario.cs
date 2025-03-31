6310f2e25b8238fb10947547c14bd1c6005770cb
Yuseeeee <49379138@est.ort.edu.ar>
2025-03-31 10:00:24 -0300

    terminado

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -12,7 +12,7 @@ public class DatosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mi nombre es" + nombreUsuario);
+        Debug.Log("Mi nombre es " + nombreUsuario + ", tengo " + edadUsuario + ", mido " + alturaUsuario + " y es " + esDonante + " que soy donante de organos");
     }
 
     // Update is called once per frame
