98ce3e0e668ad417609d9c51535f5421463db6f0
Yuseeeee <49379138@est.ort.edu.ar>
2025-03-31 09:53:25 -0300

    a

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -12,7 +12,7 @@ public class DatosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Mi nombre es" + nombreUsuario);
     }
 
     // Update is called once per frame
