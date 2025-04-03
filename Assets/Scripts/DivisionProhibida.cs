3aad2ff9ef12bdd3a985a437daab5ff24f2b0a8c
Yuseeeee <49379138@est.ort.edu.ar>
2025-04-03 09:58:23 -0300

    Division prohibida

---
+++ Assets/Scripts/DivisionProhibida.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DivisionProhibida : MonoBehaviour
+{
+
+    public float num1;
+    public float num2;
+    float resultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num2==0)
+        {
+            Debug.Log("No se puede dividir por cero");
+        }else
+        {
+            resultado = num1 / num2;
+            Debug.Log(num1 + " / " + num2 + " = " + resultado);
+        }
+        
+         
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
