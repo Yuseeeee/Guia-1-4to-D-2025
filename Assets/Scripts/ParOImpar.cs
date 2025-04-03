70ee7d805c6883ecf8043359be4fb78a46188180
Yuseeeee <49379138@est.ort.edu.ar>
2025-04-03 10:15:04 -0300

    Par o impar

---
+++ Assets/Scripts/ParOImpar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParOImpar : MonoBehaviour
+{
+    public int num1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1 > 0)
+        {
+            if (num1 % 2 == 0)
+            {
+                Debug.Log("El número es par");
+            }else
+            {
+                Debug.Log("El número es impar");
+            }
+        }
+        else
+        {
+            Debug.Log("El número debe ser mayor que 0");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
