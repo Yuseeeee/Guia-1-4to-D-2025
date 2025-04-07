88b78de8ac7a9c256684b8c82a242e38671b6b11
Yuseeeee <49379138@est.ort.edu.ar>
2025-04-07 10:41:56 -0300

    wasa

---
+++ Assets/Scripts/ComidaDeAnimalitos.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidaDeAnimalitos : MonoBehaviour
+{
+    public string codigoAnimal "";
+    int comidaPorDiaGato = 300;
+    int comidaPorDiaPerro = 400;
+    int comidaPorDiaPerroG = 700;
+    public int diasEstancia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (codigoAnimal == "G")
+        {
+
+        }
+        else if (codigoAnimal == "PP")
+            {
+
+            }
+        else if (codigoAnimal == "PG")
+                {
+
+                }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
